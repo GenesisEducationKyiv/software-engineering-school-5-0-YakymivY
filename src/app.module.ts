ff0f3404f0c2cd84a7db10637220ececb09966fb
Yuriy Yakymiv <yuriyyakymiv@Yuriys-MacBook-Air.local>
2025-05-15 16:34:22 +0300

    initialization

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { WeatherModule } from './weather/weather.module';
+import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [WeatherModule, UsersModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
