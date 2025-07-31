c13a0cf531c6c888490d8581c5af452ce3c778ad
Yuriy Yakymiv <yuriyyakymiv@Yuriys-MacBook-Air.local>
2025-07-31 13:00:25 +0300

    int and e2e tests configured

--- apps/weather-app/src/subscription/subscription.module.ts
+++ apps/weather-app/src/subscription/subscription.module.ts
@@ -53,7 +53,7 @@ import { MailEventService } from './infrastructure/services/mail-event.service';
           transport: Transport.RMQ,
           options: {
             urls: [
-              `amqp://${config.get<string>('RABBITMQ_USER')}:${config.get<string>('RABBITMQ_PASS')}@${config.get<string>('RABBITMQ_HOST')}:${config.get<string>('RABBITMQ_PORT')}`,
+              `amqp://${config.get<string>('RMQ_USER')}:${config.get<string>('RMQ_PASS')}@${config.get<string>('RMQ_HOST')}:${config.get<string>('RMQ_PORT')}`,
             ],
             queue: 'email_queue',
             queueOptions: {
